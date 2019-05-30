1898a459f3d060dd4696bfc89268baba1e160826
SangWonKo <swko1017@naver.com>
2019-05-30 14:51:28 -0700

    first commit

---
+++ Assignment2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by 고상원 on 2019-04-18.
+//  Copyright © 2019 고상원. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var purpleView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
